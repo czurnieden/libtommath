8953c0aafe75242d64720a781f8397cf9bf7e934
czurnieden <czurnieden@gmx.de>
2020-12-24 01:44:59 +0100

    Addition of faster to_radix function

--- tommath_class.h
+++ tommath_class.h
@@ -140,6 +140,7 @@
 #   define S_MP_DIV_SMALL_C
 #   define S_MP_EXPTMOD_C
 #   define S_MP_EXPTMOD_FAST_C
+#   define S_MP_FASTER_TO_RADIX_C
 #   define S_MP_GET_BIT_C
 #   define S_MP_INVMOD_C
 #   define S_MP_INVMOD_ODD_C
@@ -159,6 +160,7 @@
 #   define S_MP_RADIX_MAP_C
 #   define S_MP_RADIX_SIZE_OVERESTIMATE_C
 #   define S_MP_RAND_PLATFORM_C
+#   define S_MP_SLOWER_TO_RADIX_C
 #   define S_MP_SQR_C
 #   define S_MP_SQR_COMBA_C
 #   define S_MP_SQR_KARATSUBA_C
@@ -908,9 +910,8 @@
 #endif
 
 #if defined(MP_TO_RADIX_C)
-#   define MP_CLEAR_C
-#   define MP_DIV_D_C
-#   define MP_INIT_COPY_C
+#   define S_MP_FASTER_TO_RADIX_C
+#   define S_MP_SLOWER_TO_RADIX_C
 #endif
 
 #if defined(MP_TO_SBIN_C)
@@ -1044,6 +1045,28 @@
 #   define MP_REDUCE_2K_SETUP_C
 #   define MP_SET_C
 #   define S_MP_MONTGOMERY_REDUCE_COMBA_C
+#endif
+
+#if defined(S_MP_FASTER_TO_RADIX_C)
+#   define MP_2EXPT_C
+#   define MP_ADD_C
+#   define MP_ADD_D_C
+#   define MP_CLEAR_C
+#   define MP_CLEAR_MULTI_C
+#   define MP_CMP_C
+#   define MP_COUNT_BITS_C
+#   define MP_DIV_2D_C
+#   define MP_DIV_C
+#   define MP_EXPT_N_C
+#   define MP_INIT_C
+#   define MP_INIT_I32_C
+#   define MP_INIT_MULTI_C
+#   define MP_MUL_C
+#   define MP_SUB_C
+#   define MP_SUB_D_C
+#   define S_MP_SLOWER_TO_RADIX_C
+#   define S_MP_TO_RADIX_RECURSIVE_C
+#   define S_MP_ZERO_BUF_C
 #endif
 
 #if defined(S_MP_GET_BIT_C)
@@ -1198,6 +1221,12 @@
 #endif
 
 #if defined(S_MP_RAND_PLATFORM_C)
+#endif
+
+#if defined(S_MP_SLOWER_TO_RADIX_C)
+#   define MP_CLEAR_C
+#   define MP_DIV_D_C
+#   define MP_INIT_COPY_C
 #endif
 
 #if defined(S_MP_SQR_C)
